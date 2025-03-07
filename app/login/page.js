42f304c5c2fddc7336df5703a36814d4c1e1f004
Abalogu Winner <wabalogu@gmail.com>
2025-03-07 20:50:16 +0100

    front-end update

--- app/login/page.js
+++
@@ -1,9 +0,0 @@
-import LoginForm from '../../components/LoginForm/LoginForm';
-
-export default function LoginPage() {
-  return (
-    <div className="flex justify-center items-center h-screen bg-gray-100">
-      <LoginForm />
-    </div>
-  ); 
-}
